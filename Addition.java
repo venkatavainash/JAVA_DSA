53486c3d3bd3e69f47d038991c9ae03868206924
venkatavainash <93145195+venkatavainash@users.noreply.github.com>
2021-10-27 23:16:31 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Addition {
+
+	
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		// instantiation or object creation		
+		System.out.println("enter number1");
+		int a = obj.nextInt();		
+		System.out.println("enter number2");
+		int b = obj.nextInt();		
+		int c = a+b;		
+		System.out.println("sum value = " + c);	
+
+	}
+
+}

667386c56b476389752fd9b1b8031dd51b140fe6
venkatavainash <93145195+venkatavainash@users.noreply.github.com>
2021-10-27 23:17:25 +0530

    Add files via upload

---
+++ AreaOfSquare.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class AreaOfSquare {
+
+	
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter length of the side");
+		int a = obj.nextInt();
+	    int b = a*a;
+	    System.out.println("Area of the Square =" + b);
+	}
+
+}

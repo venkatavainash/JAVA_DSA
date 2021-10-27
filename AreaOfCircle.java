ce80715db83910adce638fd6b1b601560fd32567
venkatavainash <93145195+venkatavainash@users.noreply.github.com>
2021-10-27 23:16:47 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.*;
+
+public class AreaOfCircle {
+	public static void main(String[] args) {
+		Scanner obj =new Scanner(System.in);
+		System.out.println("enter the radius of circle :");
+		int r = obj.nextInt();
+		double areaofthecircle = 3.1415*r*r;
+		System.out.println("area of the circle :" + areaofthecircle);		
+				
+	}
+
+}

a0aa17224261aae7d0c7880a83c0a6cc89c66a8f
venkatavainash <93145195+venkatavainash@users.noreply.github.com>
2021-10-28 10:15:01 +0530

    Add files via upload

---
+++ Greeting.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Greeting {
+
+	
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+	    System.out.println("enter the input :");
+		String a = obj.nextLine();
+		System.out.println("welcome to the class " + a);
+
+	}
+
+}
